bab150be7cb9e3911736235ccef64335152313a4
Extra-codes <byaxyoxojayev@gmail.com>
2023-07-11 21:09:35 +0500

    Cool project

---
+++ apps/books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from apps.books import views
+
+urlpatterns = [
+    path('learn_once/', views.learn_once, name='learn-once'),
+    path('from_one/', views.from_one, name='from-one'),
+    path('fantasy_one/', views.fantasy_one, name='fantasy-one'),
+    path('adventure/', views.adventure, name='adventure'),
+]
--- apps/books/views.py
+++ apps/books/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def learn_once(request):
+    
+    return render(request, 'learn-once-read-everywhere/index.html')
+
+def from_one(request):
+    
+    return render(request, 'from-one-fiction-reader-to-another/index.html')
+
+def fantasy_one(request):
+    
+    return render(request, 'fantasy-in-words-and-pages/index.html')
+
+def adventure(request):
+    
+    return render(request, 'product-category/adventure-stories/index.html')
